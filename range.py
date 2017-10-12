93ada3eba2d563400c1c98897db4cd6a7cabb270
Shubham Singh <singhcse01@gmail.com>
2017-10-12 23:20:04 +0530

    Python List

---
+++ range.py
@@ -0,0 +1,24 @@
+def printsome(str):
+	print(str)
+	return
+	
+
+def changelist(list):
+	list=[1,2,3,4,5]
+	print('elements in list : ',list)
+	return
+	
+	
+list3=['you','are','great']
+
+changelist(list3)
+print('\n')
+print('element in list :' ,list3)	
+	
+printsome('my name')
+printsome('your name')
+	
+	
+	
+	
+	
